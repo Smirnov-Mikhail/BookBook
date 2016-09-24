4b3b7763bb96092adf33531fa9b47de9d2a05b91
Smirnov-Mikhail <sm_m_a@mail.ru>
2016-09-25 02:42:09 +0300

    MemBook2 -> MemBook

--- src/MemBook/Models/Order.cs
+++ src/MemBook/Models/Order.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace MemBook.Models
+﻿namespace MemBook.Models
 {
     public class Order
     {
